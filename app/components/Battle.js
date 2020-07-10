729b37bac163e53f745f69ad1ff2926bf2e78166
vvivalda <victoria.vivalda@inallmedia.com>
2020-07-10 17:10:10 -0300

    Tooltip with hover component as HOC

---
+++ app/components/Battle.js
@@ -0,0 +1,190 @@
+import React from 'react'
+import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Results from './Results'
+
+function Instructions() {
+    return (
+        <div className='instructions-container'>
+            <h1 className='center-text header-lg'>Instructions</h1>
+            <ol className='container-sm grid center-text battle-instructions'>
+                <li>
+                    <h3 className='header-sm'>Enter two Github users</h3>
+                    <FaUserFriends className='bg-light' color='rgb(255,191,116)' size={140} />
+                </li>
+                <li>
+                    <h3 className='header-sm'>Battle</h3>
+                    <FaFighterJet className='bg-light' color='#727272' size={140} />
+                </li>
+                <li>
+                    <h3 className='header-sm'>See the winner</h3>
+                    <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
+                </li>
+            </ol>
+        </div>
+    )
+}
+
+class PlayerInput extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        this.props.onSubmit(this.state.username)
+    }
+
+    render() {
+        return (
+            <form className='column player' onSubmit={this.handleSubmit}>
+                <label htmlFor='username' className='player-label'>{this.props.label}</label>
+                <div className='row player-inputs'>
+                    <input
+                        type='text'
+                        id='username'
+                        className='input-light'
+                        placeholder='github username'
+                        autoComplete='off'
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <button
+                        className='btn btn-dark'
+                        type='submit'
+                        disabled={!this.state.username}>
+                        Submit
+                    </button>
+                </div>
+            </form>
+        )
+    }
+}
+
+PlayerInput.propTypes = {
+    label: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired
+}
+
+function PlayerPreview({ username, onReset, label }) {
+    return (
+        <div className='column player'>
+            <h3 className='player-label'>{label}</h3>
+            <div className='row bg-light'>
+                <div className='player-info'>
+                    <img
+                        className='avatar-small'
+                        src={`https://github.com/${username}.png?size=200`}
+                        alt={`Avatar for ${username}`}
+                    />
+                    <a href={`https://github.com/${username}`}
+                        className='link'>
+                        {username}
+                    </a>
+                </div>
+                <button className='btn-clear flex-center' onClick={onReset}>
+                    <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+PlayerPreview.propTypes = {
+    username: PropTypes.string.isRequired,
+    onReset: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired
+}
+
+export default class Battle extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            playerOne: null,
+            playerTwo: null,
+            battle: false
+        }
+
+        this.handleReset = this.handleReset.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleReset(id) {
+        this.setState({
+            [id]: null
+        })
+    }
+    handleSubmit(id, player) {
+        this.setState({
+            [id]: player
+        })
+    }
+    render() {
+        const { playerOne, playerTwo, battle } = this.state
+
+        if (battle === true) {
+            return (
+                <Results
+                    playerOne={playerOne}
+                    playerTwo={playerTwo}
+                    onReset={() => this.setState({
+                        playerOne: null,
+                        playerTwo: null,
+                        battle: false
+                    })}
+                />
+            )
+        }
+        return (
+            <React.Fragment>
+                <Instructions />
+
+                <div className='players-container'>
+                    <h1 className='center-text header-lg'>Players</h1>
+                    <div className='row space-around'>
+                        {playerOne === null
+                            ? <PlayerInput
+                                label='Player One'
+                                onSubmit={(player) => this.handleSubmit('playerOne', player)} />
+                            : <PlayerPreview
+                                username={playerOne}
+                                label='Player One'
+                                onReset={() => this.handleReset('playerOne')} />
+                        }
+
+                        {playerTwo === null
+                            ? <PlayerInput
+                                label='Player Two'
+                                onSubmit={(player) => this.handleSubmit('playerTwo', player)} />
+                            : <PlayerPreview
+                                username={playerTwo}
+                                label='Player Two'
+                                onReset={() => this.handleReset('playerTwo')} />
+                        }
+                    </div>
+
+                    {playerOne && playerTwo && (
+                        <button
+                            className='btn btn-dark btn-space'
+                            onClick={() => { this.setState({ battle: true }) }}>
+                            Battle
+                        </button>
+                    )}
+                </div>
+            </React.Fragment>
+        )
+    }
+}
---
+++ app/utils/api.js
@@ -0,0 +1,80 @@
+const clientId = ''
+const clientSecret = ''
+const oauthParams = `?client_id=${clientId}&client_secret=${clientSecret}`
+
+function getErrorMessage(message, username) {
+    if (message === 'Not Found') {
+        return `${username} doesn't exist`
+    }
+
+    return message;
+}
+
+function getProfile(username) {
+    return fetch(`https://api.github.com/users/${username}${oauthParams}`)
+        .then((res) => res.json())
+        .then((profile) => {
+            if (profile.message) {
+                throw new Error(getErrorMessage(profile.message, username))
+            }
+
+            return profile
+        })
+}
+
+function getRepos(username) {
+    return fetch(`https://api.github.com/users/${username}/repos${oauthParams}&per_page=100`)
+        .then((res) => res.json())
+        .then((repos) => {
+            if (repos.message) {
+                throw new Error(getErrorMessage(repos.message, username))
+            }
+
+            return repos
+        })
+}
+
+function getStarCount(repos) {
+    return repos.reduce((count, { stargazers_count }) => count + stargazers_count, 0)
+}
+
+function calculateScore(followers, repos) {
+    return (followers * 3) + (getStarCount(repos) * 3.5)
+}
+
+function getUserData(player) {
+    return Promise.all([
+        getProfile(player),
+        getRepos(player)
+    ]).then(([profile, repos]) => ({
+        profile,
+        score: calculateScore(profile.followers, repos)
+    }))
+}
+
+function sortPlayers(players) {
+    return players.sort((a, b) => b.score - a.score)
+}
+
+export function battle(players) {
+    return Promise.all([
+        getUserData(players[0]),
+        getUserData(players[1])
+    ]).then((results) => sortPlayers(results))
+}
+
+export function fetchPopularRepos(language) {
+    const endpoint = window.encodeURI(
+        `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+    )
+    return fetch(endpoint)
+        .then((res) => res.json())
+        .then((data) => {
+            if (!data.items) {
+                throw new Error(data.message)
+            }
+
+            return data.items
+        })
+
+}
