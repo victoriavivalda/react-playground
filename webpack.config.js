371ca828bd1e99f636f681ecff97c861f8005a88
vvivalda <victoria.vivalda@inallmedia.com>
2020-07-13 15:56:40 -0300

    prepare production build

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
             template: './app/index.html'
         })
     ],
-    mode: 'development',
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     devServer: {
         historyApiFallback: true
     }
